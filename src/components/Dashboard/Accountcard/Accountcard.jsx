889c80b8a6d0b92632112635e030fd418709a205
Jesus Chavez <jhco1960@gmail.com>
2022-07-12 16:24:16 -0600

    Loaded from Ubuntu new machine

--- src/components/Dashboard/Accountcard/Accountcard.jsx
+++ src/components/Dashboard/Accountcard/Accountcard.jsx
@@ -1,34 +1,35 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button';
-import { DotsHorizontalIcon, CurrencyDollarIcon, PlusIcon } from '@heroicons/react/solid'
+import { DotsHorizontalIcon, CurrencyDollarIcon, PlusIcon } from '@heroicons/react/solid';
+import Transactionmodal from '../TransactionModal/Transactionmodal';
 import './accountcard.css'
 
 
 const Accountcard = ({ plus }) => {
     let accountName = 'Account1';
 
-    if(plus){
-        return(
-            <Card>
-            <Card.Header className='headerPlus'>
-                <span>New Account</span>
-            </Card.Header>
-            <Card.Body>
-                <Card.Title className='text-center'><PlusIcon class="plusIcon"/></Card.Title>
-            </Card.Body>
-        </Card>
+   
+    if (plus) {
+        return (
+            <Card className='cardAccount accountDashboard'>
+                <Card.Header className='headerPlus'>
+                    <span>New Account</span>
+                </Card.Header>
+                <Card.Body className='cardBodyAccount'>
+                    <Card.Title className='text-center'><PlusIcon className="plusIcon" /></Card.Title>
+                </Card.Body>
+            </Card>
         );
     }
 
     return (
-        <Card>
-            <Card.Header>
-                <span>Account 1</span> 
-                <DotsHorizontalIcon className='dotsIcon'/>
+        <Card className='cardAccount'> 
+            <Card.Header className='headerAccount'>
+                <span>{accountName}</span>                
+                <Transactionmodal account={accountName}><DotsHorizontalIcon className='dotsIcon' /></Transactionmodal>
             </Card.Header>
-            <Card.Body>
-                <Card.Title className='text-center'><CurrencyDollarIcon class="dollarIcon"/> USD 110.00</Card.Title>
+            <Card.Body className='cardBodyAccount'>
+                <Card.Title className='text-center'><CurrencyDollarIcon className="dollarIcon" /> USD 110.00</Card.Title>
             </Card.Body>
         </Card>
     );
