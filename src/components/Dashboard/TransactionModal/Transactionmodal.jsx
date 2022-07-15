42c2f9621149280d362aec7f75a4eb7b8c5a4215
Jesus Chavez <jhco1960@gmail.com>
2022-07-15 16:07:44 -0600

    Transactions cards

--- src/components/Dashboard/TransactionModal/Transactionmodal.jsx
+++ src/components/Dashboard/TransactionModal/Transactionmodal.jsx
@@ -1,52 +1,73 @@
 import React from 'react';
 import Modal from 'react-bootstrap/Modal';
+import Tab from 'react-bootstrap/Tab';
+import Tabs from 'react-bootstrap/Tabs';
 import { DotsHorizontalIcon } from '@heroicons/react/solid';
 import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import '../Accountcard/accountcard.css';
+import './Transactionmodal.css';
+//import Card from 'react-bootstrap/Card';
+
+/* Components */
+import Addcard from '../Transactioncards/Addcard';
 
 
 const Transactionmodal = (props) => {
-    
+
     const account = props.account;
     const [modalShow, setModalShow] = useState(false);
 
     return (
         <>
             <Button className='dotsAccountButton' onClick={() => setModalShow(true)}>
-            <DotsHorizontalIcon className='dotsIcon' />
+                <DotsHorizontalIcon className='dotsIcon' />
             </Button>
 
             <MyVerticallyCenteredModal
-                account= {account}
+                account={account}
                 show={modalShow}
                 onHide={() => setModalShow(false)}
+                backdrop="static"
+                
             />
         </>
     )
 }
 
 const MyVerticallyCenteredModal = (props) => {
-    
+
     return (
         <Modal
             {...props}
-            size="lg"
+            size="md"
             aria-labelledby="contained-modal-title-vcenter"
             centered
         >
-            <Modal.Header closeButton>
+            <Modal.Header className='modalHeader'>
                 <Modal.Title id="contained-modal-title-vcenter">
                     {props.account}
                 </Modal.Title>
             </Modal.Header>
             <Modal.Body>
-                <h4>Centered Modal</h4>
-                <p>
-                    Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-                    dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
-                    consectetur ac, vestibulum at eros.
-                </p>
+            <Tabs
+                    defaultActiveKey="profile"
+                    id="fill-tab-example"
+                    fill
+                >
+                    <Tab eventKey="home" title="Home">
+                        <Addcard/>
+                    </Tab>
+                    <Tab eventKey="profile" title="Profile">
+
+                    </Tab>
+                    <Tab eventKey="longer-tab" title="Loooonger Tab">
+
+                    </Tab>
+                    <Tab eventKey="contact" title="Contact">
+
+                    </Tab>
+                </Tabs>
             </Modal.Body>
             <Modal.Footer>
                 <Button onClick={props.onHide}>Close</Button>
@@ -54,5 +75,11 @@ const MyVerticallyCenteredModal = (props) => {
         </Modal>
     );
 }
+
+// const Income = () => {
+//     return(
+        
+//     );
+// }
 
 export default Transactionmodal;
